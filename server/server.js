9fe877b5d918311dbb5705dbd559eff3e47e33c8
yoshiyuki.mizogami <mizogami00@gmail.com>
2020-11-20 20:51:20 +0900

    port change

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const ex = require('express')
 const app = ex()
 
 
-const port = process.env.PORT | 8080
+const port = process.env.PORT | 5000
 app.use(ex.static('public')).listen(port)
 console.log(`http://localhost:${port}`)
