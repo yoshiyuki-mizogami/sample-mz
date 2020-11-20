8346d5754fdc61b0d41fa4d2204a7a14fefb3571
yoshiyuki.mizogami <mizogami00@gmail.com>
2020-11-20 20:40:28 +0900

    fix

--- server/server.js
+++ server/server.js
@@ -3,4 +3,6 @@ const ex = require('express')
 const app = ex()
 
 
-app.use(ex.static('public'))
+const port = process.env.PORT | 8080
+app.use(ex.static('public')).listen(port)
+console.log(`http://localhost:${port}`)
